b42f797cdc3076fec72aab7bc4ab6c06565dc8c7
Samuel Salgado Gonzalez <vesuvianmetal@hotmail.com>
2017-04-06 13:01:55 -0700

    hola

--- ClimateReports/ClimateReports/Form1.Designer.cs
+++ ClimateReports/ClimateReports/Form1.Designer.cs
@@ -45,12 +45,12 @@
             // 
             this.txtusuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.txtusuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtusuario.Location = new System.Drawing.Point(59, 56);
-            this.txtusuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtusuario.Location = new System.Drawing.Point(84, 69);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtusuario.MaxLength = 20;
             this.txtusuario.Multiline = true;
             this.txtusuario.Name = "txtusuario";
-            this.txtusuario.Size = new System.Drawing.Size(192, 41);
+            this.txtusuario.Size = new System.Drawing.Size(255, 50);
             this.txtusuario.TabIndex = 4;
             this.txtusuario.TextChanged += new System.EventHandler(this.txtusuario_TextChanged);
             // 
@@ -60,9 +60,10 @@
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::ClimateReports.Properties.Resources.usuario_1;
-            this.button1.Location = new System.Drawing.Point(10, 54);
+            this.button1.Location = new System.Drawing.Point(13, 66);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(48, 43);
+            this.button1.Size = new System.Drawing.Size(64, 53);
             this.button1.TabIndex = 8;
             this.button1.UseVisualStyleBackColor = false;
             // 
@@ -72,9 +73,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(54, 17);
+            this.label1.Location = new System.Drawing.Point(72, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 31);
+            this.label1.Size = new System.Drawing.Size(135, 39);
             this.label1.TabIndex = 10;
             this.label1.Text = "Usuario";
             // 
@@ -84,9 +86,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(54, 107);
+            this.label2.Location = new System.Drawing.Point(72, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.Size = new System.Drawing.Size(167, 39);
             this.label2.TabIndex = 11;
             this.label2.Text = "Password";
             // 
@@ -97,9 +100,10 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Image = global::ClimateReports.Properties.Resources.iniciar_sesion_21;
-            this.button3.Location = new System.Drawing.Point(104, 194);
+            this.button3.Location = new System.Drawing.Point(139, 239);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 36);
+            this.button3.Size = new System.Drawing.Size(108, 44);
             this.button3.TabIndex = 12;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
@@ -110,9 +114,10 @@
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::ClimateReports.Properties.Resources.palabras_clave;
-            this.button2.Location = new System.Drawing.Point(10, 146);
+            this.button2.Location = new System.Drawing.Point(13, 180);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(48, 43);
+            this.button2.Size = new System.Drawing.Size(64, 53);
             this.button2.TabIndex = 13;
             this.button2.UseVisualStyleBackColor = false;
             // 
@@ -128,19 +133,20 @@
             this.panel1.Controls.Add(this.txtusuario);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(85, 201);
+            this.panel1.Location = new System.Drawing.Point(113, 247);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(270, 297);
+            this.panel1.Size = new System.Drawing.Size(360, 366);
             this.panel1.TabIndex = 14;
             // 
             // txtcontra
             // 
             this.txtcontra.AcceptsTab = true;
             this.txtcontra.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtcontra.Location = new System.Drawing.Point(59, 149);
-            this.txtcontra.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtcontra.Location = new System.Drawing.Point(79, 183);
+            this.txtcontra.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtcontra.Name = "txtcontra";
-            this.txtcontra.Size = new System.Drawing.Size(192, 37);
+            this.txtcontra.Size = new System.Drawing.Size(255, 45);
             this.txtcontra.TabIndex = 5;
             this.txtcontra.UseSystemPasswordChar = true;
             // 
@@ -149,9 +155,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(18, 243);
+            this.label4.Location = new System.Drawing.Point(24, 299);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 16);
+            this.label4.Size = new System.Drawing.Size(178, 20);
             this.label4.TabIndex = 15;
             this.label4.Text = "Recuperar Contraseña";
             // 
@@ -160,20 +167,21 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(18, 267);
+            this.label3.Location = new System.Drawing.Point(24, 329);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.Size = new System.Drawing.Size(64, 20);
             this.label3.TabIndex = 14;
             this.label3.Text = "Ayuda?";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ClimateReports.Properties.Resources.fondologinnn;
-            this.ClientSize = new System.Drawing.Size(439, 510);
+            this.ClientSize = new System.Drawing.Size(585, 628);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
