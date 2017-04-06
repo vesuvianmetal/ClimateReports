2df5eff360626b1ca49587a06632d5c63c44f19e
Samuel Salgado Gonzalez <vesuvianmetal@hotmail.com>
2017-04-05 21:53:28 -0700

    commit

--- ClimateReports/ClimateReports/Form1.Designer.cs
+++ ClimateReports/ClimateReports/Form1.Designer.cs
@@ -36,32 +36,33 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtusuario
             // 
             this.txtusuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.txtusuario.Location = new System.Drawing.Point(60, 148);
-            this.txtusuario.Margin = new System.Windows.Forms.Padding(2);
+            this.txtusuario.Location = new System.Drawing.Point(79, 69);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtusuario.MaxLength = 20;
             this.txtusuario.Multiline = true;
             this.txtusuario.Name = "txtusuario";
-            this.txtusuario.Size = new System.Drawing.Size(192, 41);
+            this.txtusuario.Size = new System.Drawing.Size(255, 50);
             this.txtusuario.TabIndex = 4;
             this.txtusuario.TextChanged += new System.EventHandler(this.txtusuario_TextChanged);
             // 
             // txtcontra
             // 
             this.txtcontra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.txtcontra.Location = new System.Drawing.Point(60, 55);
-            this.txtcontra.Margin = new System.Windows.Forms.Padding(2);
-            this.txtcontra.MaxLength = 45;
+            this.txtcontra.Location = new System.Drawing.Point(79, 183);
+            this.txtcontra.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtcontra.MaxLength = 20;
             this.txtcontra.Multiline = true;
             this.txtcontra.Name = "txtcontra";
-            this.txtcontra.Size = new System.Drawing.Size(192, 41);
+            this.txtcontra.PasswordChar = '*';
+            this.txtcontra.Size = new System.Drawing.Size(255, 50);
             this.txtcontra.TabIndex = 5;
             this.txtcontra.UseSystemPasswordChar = true;
             // 
@@ -71,9 +72,10 @@
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::ClimateReports.Properties.Resources.usuario_1;
-            this.button1.Location = new System.Drawing.Point(10, 54);
+            this.button1.Location = new System.Drawing.Point(13, 66);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(48, 43);
+            this.button1.Size = new System.Drawing.Size(64, 53);
             this.button1.TabIndex = 8;
             this.button1.UseVisualStyleBackColor = false;
             // 
@@ -81,11 +83,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Unified", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(54, 17);
+            this.label1.Location = new System.Drawing.Point(72, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 34);
+            this.label1.Size = new System.Drawing.Size(129, 39);
             this.label1.TabIndex = 10;
             this.label1.Text = "usuario";
             // 
@@ -93,11 +96,12 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Unified", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(54, 107);
+            this.label2.Location = new System.Drawing.Point(72, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 34);
+            this.label2.Size = new System.Drawing.Size(163, 39);
             this.label2.TabIndex = 11;
             this.label2.Text = "password";
             // 
@@ -106,11 +110,12 @@
             this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Unified", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Image = global::ClimateReports.Properties.Resources.iniciar_sesion_21;
-            this.button3.Location = new System.Drawing.Point(104, 194);
+            this.button3.Location = new System.Drawing.Point(139, 239);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 36);
+            this.button3.Size = new System.Drawing.Size(108, 44);
             this.button3.TabIndex = 12;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
@@ -121,9 +126,10 @@
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::ClimateReports.Properties.Resources.palabras_clave;
-            this.button2.Location = new System.Drawing.Point(10, 146);
+            this.button2.Location = new System.Drawing.Point(13, 180);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(48, 43);
+            this.button2.Size = new System.Drawing.Size(64, 53);
             this.button2.TabIndex = 13;
             this.button2.UseVisualStyleBackColor = false;
             // 
@@ -131,49 +137,52 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtcontra);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.txtcontra);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.txtusuario);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(85, 201);
+            this.panel1.Location = new System.Drawing.Point(113, 247);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(270, 297);
+            this.panel1.Size = new System.Drawing.Size(360, 366);
             this.panel1.TabIndex = 14;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Unified", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(18, 267);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 16);
-            this.label3.TabIndex = 14;
-            this.label3.Text = "Ayuda?";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Unified", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(18, 243);
+            this.label4.Location = new System.Drawing.Point(24, 299);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(137, 16);
+            this.label4.Size = new System.Drawing.Size(178, 20);
             this.label4.TabIndex = 15;
             this.label4.Text = "Recuperar Contraseña";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(24, 329);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Ayuda?";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ClimateReports.Properties.Resources.fondologinnn;
-            this.ClientSize = new System.Drawing.Size(439, 510);
+            this.ClientSize = new System.Drawing.Size(585, 628);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
--- ClimateReports/ClimateReports/Form1.cs
+++ ClimateReports/ClimateReports/Form1.cs
@@ -37,91 +37,7 @@ namespace ClimateReports
         private void btniniciar_Click(object sender, EventArgs e)
         {
            
-            ///validacion de campos
-            ///valida si uno o ambos campos del login estan vacios.
-            if (txtusuario.Text.Equals("") || txtcontra.Text.Equals(""))
-            {
-                ///muestra ventana con error si se presenta la validacion.
-                MessageBox.Show("Usuario O Contraseña Invalida, Porfavor Intente Denuevo");
-            }
-
-            ///si los campos no presentan error de que esten vacios se procede a correr este codigo
-            else
-            {
-                ///con.open() sirve para abrir la conexion a la base de datos.
-                conn.Open();
             
-                ///string query inicio es una declaracion de el query(CONSULTA) que se va a realizar a la base de datos.
-            string query_inicio = "select * from usuario where USU_Usuario = '" + txtusuario.Text + "' AND USU_PASS ='" + txtcontra.Text + "'";
-
-                ///mysql command exe_query sirve como preparacion del query(CONSULTA) junto con la variable de conexion
-                ///para asi poder convertirlo en un comando a realizar.
-                MySqlCommand exe_query_inicio = new MySqlCommand(query_inicio, conn);
-
-                ///string cap es la declaracion de variable que servira para capturar un valor dentro
-                ///de la base de datos.
-                string cap;
-
-
-                
-            MySqlDataReader leer_exe;
-
-            try
-            {
-                    ///de establece la orden de ejecucion de query (CONSULTAS)
-                leer_exe = exe_query_inicio.ExecuteReader();
-
-
-                    ///validacion
-                    ///validacion de usuario existente
-                if (leer_exe.Read())
-                {
-
-                    MessageBox.Show("CONECTADO");
-
-                        ///se le indica a la variable cap que valor de la columna va a obtener, el (8) significa el numero
-                        ///de columna que va a obtener
-                    cap = leer_exe.GetString("USU_TIPO_USU");
-
-                        ///si existe el usuario, verifica con la variable cap que tipo de usuario es
-                        ///y dependiendo el usuario, le muestra la ventana a la que tiene permisos de acceder
-                    if (cap.Equals("Admin"))
-                    {
-                        Reporte_Detallado RD = new Reporte_Detallado();
-                        RD.Show();
-                        this.Hide();
-                    }
-                    else if (cap.Equals("General"))
-                    {
-                        Reporte_General RG = new Reporte_General();
-                        RG.Show();
-                        this.Hide();
-                    }
-                }
-
-                /// si el usuario no existe o la contraseña es erronea entonces muestra mensaje de error
-                else if (leer_exe.Read() == false)
-                {
-                        ///mensaje de error que se mostrara
-                    MessageBox.Show("Inicio Fallido, Usuario Y Contraseña O La Conexion");
-
-                }
-
-
-
-
-
-            }
-
-                ///si hay algun error del codigo, muestra un error con su causante.
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-                ///conn.close() sirve para cerrar la conexion a la base de datos
-            conn.Close();
-        }
              }
 
         private void txtusuario_TextChanged(object sender, EventArgs e)
@@ -136,8 +52,93 @@ namespace ClimateReports
 
         private void button3_Click_1(object sender, EventArgs e)
         {
-            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-            this.BackColor = Color.Transparent;
+           
+
+            ///validacion de campos
+            ///valida si uno o ambos campos del login estan vacios.
+            if (txtusuario.Text.Equals("") || txtcontra.Text.Equals(""))
+            {
+                ///muestra ventana con error si se presenta la validacion.
+                MessageBox.Show("Usuario O Contraseña Invalida, Porfavor Intente Denuevo");
+            }
+
+            ///si los campos no presentan error de que esten vacios se procede a correr este codigo
+            else
+            {
+                ///con.open() sirve para abrir la conexion a la base de datos.
+                conn.Open();
+
+                ///string query inicio es una declaracion de el query(CONSULTA) que se va a realizar a la base de datos.
+                string query_inicio = "select * from usuario where USU_Usuario = '" + txtusuario.Text + "' AND USU_PASS ='" + txtcontra.Text + "'";
+
+                ///mysql command exe_query sirve como preparacion del query(CONSULTA) junto con la variable de conexion
+                ///para asi poder convertirlo en un comando a realizar.
+                MySqlCommand exe_query_inicio = new MySqlCommand(query_inicio, conn);
+
+                ///string cap es la declaracion de variable que servira para capturar un valor dentro
+                ///de la base de datos.
+                string cap;
+
+
+
+                MySqlDataReader leer_exe;
+
+                try
+                {
+                    ///de establece la orden de ejecucion de query (CONSULTAS)
+                    leer_exe = exe_query_inicio.ExecuteReader();
+
+
+                    ///validacion
+                    ///validacion de usuario existente
+                    if (leer_exe.Read())
+                    {
+
+                        MessageBox.Show("CONECTADO");
+
+                        ///se le indica a la variable cap que valor de la columna va a obtener, el (8) significa el numero
+                        ///de columna que va a obtener
+                        cap = leer_exe.GetString("USU_TIPO_USU");
+
+                        ///si existe el usuario, verifica con la variable cap que tipo de usuario es
+                        ///y dependiendo el usuario, le muestra la ventana a la que tiene permisos de acceder
+                        if (cap.Equals("Admin"))
+                        {
+                            Reporte_Detallado RD = new Reporte_Detallado();
+                            RD.Show();
+                            this.Hide();
+                        }
+                        else if (cap.Equals("General"))
+                        {
+                            Reporte_General RG = new Reporte_General();
+                            RG.Show();
+                            this.Hide();
+                        }
+                    }
+
+                    /// si el usuario no existe o la contraseña es erronea entonces muestra mensaje de error
+                    else if (leer_exe.Read() == false)
+                    {
+                        ///mensaje de error que se mostrara
+                        MessageBox.Show("Inicio Fallido, Usuario Y Contraseña O La Conexion");
+
+                    }
+
+
+
+
+
+                }
+
+                ///si hay algun error del codigo, muestra un error con su causante.
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                ///conn.close() sirve para cerrar la conexion a la base de datos
+                conn.Close();
+            }
         }
     }
 }
