756854a7d82c38db063c8258220f87e47e11e8d9
Brend <Brend@BRENDA-BC>
2017-04-06 13:09:03 -0600

    4hj46thj

--- ClimateReports/ClimateReports/Reporte_Detallado.Designer.cs
+++ ClimateReports/ClimateReports/Reporte_Detallado.Designer.cs
@@ -63,7 +63,8 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(771, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(577, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -75,34 +76,34 @@
             this.humedadToolStripMenuItem,
             this.presionToolStripMenuItem});
             this.modulosToolStripMenuItem.Name = "modulosToolStripMenuItem";
-            this.modulosToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.modulosToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
             this.modulosToolStripMenuItem.Text = "Modulos";
             // 
             // tempraturaToolStripMenuItem
             // 
             this.tempraturaToolStripMenuItem.Name = "tempraturaToolStripMenuItem";
-            this.tempraturaToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.tempraturaToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.tempraturaToolStripMenuItem.Text = "Tempratura";
             this.tempraturaToolStripMenuItem.Click += new System.EventHandler(this.tempraturaToolStripMenuItem_Click);
             // 
             // vientoToolStripMenuItem
             // 
             this.vientoToolStripMenuItem.Name = "vientoToolStripMenuItem";
-            this.vientoToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.vientoToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.vientoToolStripMenuItem.Text = "Viento";
             this.vientoToolStripMenuItem.Click += new System.EventHandler(this.vientoToolStripMenuItem_Click);
             // 
             // humedadToolStripMenuItem
             // 
             this.humedadToolStripMenuItem.Name = "humedadToolStripMenuItem";
-            this.humedadToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.humedadToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.humedadToolStripMenuItem.Text = "Humedad";
             this.humedadToolStripMenuItem.Click += new System.EventHandler(this.humedadToolStripMenuItem_Click);
             // 
             // presionToolStripMenuItem
             // 
             this.presionToolStripMenuItem.Name = "presionToolStripMenuItem";
-            this.presionToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.presionToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.presionToolStripMenuItem.Text = "Presion";
             this.presionToolStripMenuItem.Click += new System.EventHandler(this.presionToolStripMenuItem_Click);
             // 
@@ -111,119 +112,132 @@
             this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mesaDeAyudaToolStripMenuItem});
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             // 
             // mesaDeAyudaToolStripMenuItem
             // 
             this.mesaDeAyudaToolStripMenuItem.Name = "mesaDeAyudaToolStripMenuItem";
-            this.mesaDeAyudaToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.mesaDeAyudaToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.mesaDeAyudaToolStripMenuItem.Text = "Mesa De Ayuda";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 54);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(733, 282);
+            this.dataGridView1.Size = new System.Drawing.Size(550, 229);
             this.dataGridView1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(262, 32);
+            this.label1.Size = new System.Drawing.Size(204, 26);
             this.label1.TabIndex = 2;
             this.label1.Text = "Reporte Detallado";
             // 
             // txttemperatura
             // 
-            this.txttemperatura.Location = new System.Drawing.Point(144, 486);
+            this.txttemperatura.Location = new System.Drawing.Point(108, 395);
+            this.txttemperatura.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txttemperatura.Name = "txttemperatura";
-            this.txttemperatura.Size = new System.Drawing.Size(100, 22);
+            this.txttemperatura.Size = new System.Drawing.Size(76, 20);
             this.txttemperatura.TabIndex = 21;
             // 
             // txtpresion
             // 
-            this.txtpresion.Location = new System.Drawing.Point(144, 457);
+            this.txtpresion.Location = new System.Drawing.Point(108, 371);
+            this.txtpresion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtpresion.Name = "txtpresion";
-            this.txtpresion.Size = new System.Drawing.Size(100, 22);
+            this.txtpresion.Size = new System.Drawing.Size(76, 20);
             this.txtpresion.TabIndex = 20;
             // 
             // txtviento
             // 
-            this.txtviento.Location = new System.Drawing.Point(144, 427);
+            this.txtviento.Location = new System.Drawing.Point(108, 347);
+            this.txtviento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtviento.Name = "txtviento";
-            this.txtviento.Size = new System.Drawing.Size(100, 22);
+            this.txtviento.Size = new System.Drawing.Size(76, 20);
             this.txtviento.TabIndex = 19;
             // 
             // txtsensor
             // 
-            this.txtsensor.Location = new System.Drawing.Point(144, 371);
+            this.txtsensor.Location = new System.Drawing.Point(108, 301);
+            this.txtsensor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtsensor.Name = "txtsensor";
-            this.txtsensor.Size = new System.Drawing.Size(100, 22);
+            this.txtsensor.Size = new System.Drawing.Size(76, 20);
             this.txtsensor.TabIndex = 18;
             // 
             // txtporhumedad
             // 
-            this.txtporhumedad.Location = new System.Drawing.Point(144, 397);
+            this.txtporhumedad.Location = new System.Drawing.Point(108, 323);
+            this.txtporhumedad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtporhumedad.Name = "txtporhumedad";
-            this.txtporhumedad.Size = new System.Drawing.Size(100, 22);
+            this.txtporhumedad.Size = new System.Drawing.Size(76, 20);
             this.txtporhumedad.TabIndex = 17;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(14, 478);
+            this.label6.Location = new System.Drawing.Point(10, 388);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.Size = new System.Drawing.Size(70, 13);
             this.label6.TabIndex = 16;
             this.label6.Text = "Temperatura:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 457);
+            this.label5.Location = new System.Drawing.Point(10, 371);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.Size = new System.Drawing.Size(45, 13);
             this.label5.TabIndex = 15;
             this.label5.Text = "Presion:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 427);
+            this.label4.Location = new System.Drawing.Point(8, 347);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.Size = new System.Drawing.Size(40, 13);
             this.label4.TabIndex = 14;
             this.label4.Text = "Viento:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 397);
+            this.label3.Location = new System.Drawing.Point(10, 323);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 13;
             this.label3.Text = "% Humedad:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 371);
+            this.label2.Location = new System.Drawing.Point(10, 301);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 12;
             this.label2.Text = "Sensor:";
             // 
             // btncomentario
             // 
-            this.btncomentario.Location = new System.Drawing.Point(351, 534);
+            this.btncomentario.Location = new System.Drawing.Point(263, 434);
+            this.btncomentario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btncomentario.Name = "btncomentario";
-            this.btncomentario.Size = new System.Drawing.Size(75, 23);
+            this.btncomentario.Size = new System.Drawing.Size(56, 19);
             this.btncomentario.TabIndex = 24;
             this.btncomentario.Text = "Enviar";
             this.btncomentario.UseVisualStyleBackColor = true;
@@ -231,25 +245,27 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(351, 371);
+            this.label7.Location = new System.Drawing.Point(263, 301);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(80, 17);
+            this.label7.Size = new System.Drawing.Size(60, 13);
             this.label7.TabIndex = 23;
             this.label7.Text = "Comentario";
             // 
             // txtcomentario
             // 
-            this.txtcomentario.Location = new System.Drawing.Point(351, 394);
+            this.txtcomentario.Location = new System.Drawing.Point(263, 320);
+            this.txtcomentario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtcomentario.Multiline = true;
             this.txtcomentario.Name = "txtcomentario";
-            this.txtcomentario.Size = new System.Drawing.Size(394, 134);
+            this.txtcomentario.Size = new System.Drawing.Size(296, 110);
             this.txtcomentario.TabIndex = 22;
             // 
             // Reporte_Detallado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(771, 578);
+            this.ClientSize = new System.Drawing.Size(577, 470);
             this.Controls.Add(this.btncomentario);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtcomentario);
@@ -267,6 +283,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Reporte_Detallado";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reporte Detallado";
