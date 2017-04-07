3a8d92612c7eecc079ea28ca9297b0f379aad4f8
pinopc <pinopc@PHINOPC>
2017-04-06 19:41:38 -0600

    Diseo del reporte detallado!

--- ClimateReports/ClimateReports/Reporte_Detallado.Designer.cs
+++ ClimateReports/ClimateReports/Reporte_Detallado.Designer.cs
@@ -29,14 +29,6 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.modulosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tempraturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.vientoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.humedadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.presionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mesaDeAyudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.txttemperatura = new System.Windows.Forms.TextBox();
             this.txtpresion = new System.Windows.Forms.TextBox();
@@ -51,101 +43,41 @@
             this.btncomentario = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.txtcomentario = new System.Windows.Forms.TextBox();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportarUnProblemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mesaDeAyudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proporcionarUnaSugerenciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.soporteTecnicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.modulosToolStripMenuItem,
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(577, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(795, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // modulosToolStripMenuItem
-            // 
-            this.modulosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tempraturaToolStripMenuItem,
-            this.vientoToolStripMenuItem,
-            this.humedadToolStripMenuItem,
-            this.presionToolStripMenuItem});
-            this.modulosToolStripMenuItem.Name = "modulosToolStripMenuItem";
-            this.modulosToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
-            this.modulosToolStripMenuItem.Text = "Modulos";
-            // 
-            // tempraturaToolStripMenuItem
-            // 
-            this.tempraturaToolStripMenuItem.Name = "tempraturaToolStripMenuItem";
-            this.tempraturaToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
-            this.tempraturaToolStripMenuItem.Text = "Tempratura";
-            this.tempraturaToolStripMenuItem.Click += new System.EventHandler(this.tempraturaToolStripMenuItem_Click);
-            // 
-            // vientoToolStripMenuItem
-            // 
-            this.vientoToolStripMenuItem.Name = "vientoToolStripMenuItem";
-            this.vientoToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
-            this.vientoToolStripMenuItem.Text = "Viento";
-            this.vientoToolStripMenuItem.Click += new System.EventHandler(this.vientoToolStripMenuItem_Click);
-            // 
-            // humedadToolStripMenuItem
-            // 
-            this.humedadToolStripMenuItem.Name = "humedadToolStripMenuItem";
-            this.humedadToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
-            this.humedadToolStripMenuItem.Text = "Humedad";
-            this.humedadToolStripMenuItem.Click += new System.EventHandler(this.humedadToolStripMenuItem_Click);
-            // 
-            // presionToolStripMenuItem
-            // 
-            this.presionToolStripMenuItem.Name = "presionToolStripMenuItem";
-            this.presionToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
-            this.presionToolStripMenuItem.Text = "Presion";
-            this.presionToolStripMenuItem.Click += new System.EventHandler(this.presionToolStripMenuItem_Click);
-            // 
-            // ayudaToolStripMenuItem
-            // 
-            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mesaDeAyudaToolStripMenuItem});
-            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-            this.ayudaToolStripMenuItem.Text = "Ayuda";
-            // 
-            // mesaDeAyudaToolStripMenuItem
-            // 
-            this.mesaDeAyudaToolStripMenuItem.Name = "mesaDeAyudaToolStripMenuItem";
-            this.mesaDeAyudaToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
-            this.mesaDeAyudaToolStripMenuItem.Text = "Mesa De Ayuda";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 54);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(550, 229);
-            this.dataGridView1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Font = new System.Drawing.Font("Unified", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 42);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 26);
+            this.label1.Size = new System.Drawing.Size(209, 29);
             this.label1.TabIndex = 2;
             this.label1.Text = "Reporte Detallado";
             // 
             // txttemperatura
             // 
             this.txttemperatura.Location = new System.Drawing.Point(108, 395);
-            this.txttemperatura.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txttemperatura.Margin = new System.Windows.Forms.Padding(2);
             this.txttemperatura.Name = "txttemperatura";
             this.txttemperatura.Size = new System.Drawing.Size(76, 20);
             this.txttemperatura.TabIndex = 21;
@@ -153,7 +85,7 @@
             // txtpresion
             // 
             this.txtpresion.Location = new System.Drawing.Point(108, 371);
-            this.txtpresion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtpresion.Margin = new System.Windows.Forms.Padding(2);
             this.txtpresion.Name = "txtpresion";
             this.txtpresion.Size = new System.Drawing.Size(76, 20);
             this.txtpresion.TabIndex = 20;
@@ -161,7 +93,7 @@
             // txtviento
             // 
             this.txtviento.Location = new System.Drawing.Point(108, 347);
-            this.txtviento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtviento.Margin = new System.Windows.Forms.Padding(2);
             this.txtviento.Name = "txtviento";
             this.txtviento.Size = new System.Drawing.Size(76, 20);
             this.txtviento.TabIndex = 19;
@@ -169,7 +101,7 @@
             // txtsensor
             // 
             this.txtsensor.Location = new System.Drawing.Point(108, 301);
-            this.txtsensor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtsensor.Margin = new System.Windows.Forms.Padding(2);
             this.txtsensor.Name = "txtsensor";
             this.txtsensor.Size = new System.Drawing.Size(76, 20);
             this.txtsensor.TabIndex = 18;
@@ -177,7 +109,7 @@
             // txtporhumedad
             // 
             this.txtporhumedad.Location = new System.Drawing.Point(108, 323);
-            this.txtporhumedad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtporhumedad.Margin = new System.Windows.Forms.Padding(2);
             this.txtporhumedad.Name = "txtporhumedad";
             this.txtporhumedad.Size = new System.Drawing.Size(76, 20);
             this.txtporhumedad.TabIndex = 17;
@@ -235,7 +167,7 @@
             // btncomentario
             // 
             this.btncomentario.Location = new System.Drawing.Point(263, 434);
-            this.btncomentario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btncomentario.Margin = new System.Windows.Forms.Padding(2);
             this.btncomentario.Name = "btncomentario";
             this.btncomentario.Size = new System.Drawing.Size(56, 19);
             this.btncomentario.TabIndex = 24;
@@ -255,17 +187,54 @@
             // txtcomentario
             // 
             this.txtcomentario.Location = new System.Drawing.Point(263, 320);
-            this.txtcomentario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtcomentario.Margin = new System.Windows.Forms.Padding(2);
             this.txtcomentario.Multiline = true;
             this.txtcomentario.Name = "txtcomentario";
             this.txtcomentario.Size = new System.Drawing.Size(296, 110);
             this.txtcomentario.TabIndex = 22;
             // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reportarUnProblemaToolStripMenuItem,
+            this.mesaDeAyudaToolStripMenuItem,
+            this.proporcionarUnaSugerenciaToolStripMenuItem,
+            this.soporteTecnicoToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Font = new System.Drawing.Font("Unified", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ayudaToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // reportarUnProblemaToolStripMenuItem
+            // 
+            this.reportarUnProblemaToolStripMenuItem.Name = "reportarUnProblemaToolStripMenuItem";
+            this.reportarUnProblemaToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.reportarUnProblemaToolStripMenuItem.Text = "Ver la Ayuda";
+            // 
+            // mesaDeAyudaToolStripMenuItem
+            // 
+            this.mesaDeAyudaToolStripMenuItem.Name = "mesaDeAyudaToolStripMenuItem";
+            this.mesaDeAyudaToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.mesaDeAyudaToolStripMenuItem.Text = "Notificar un Problema";
+            // 
+            // proporcionarUnaSugerenciaToolStripMenuItem
+            // 
+            this.proporcionarUnaSugerenciaToolStripMenuItem.Name = "proporcionarUnaSugerenciaToolStripMenuItem";
+            this.proporcionarUnaSugerenciaToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.proporcionarUnaSugerenciaToolStripMenuItem.Text = "Proporcionar una Sugerencia";
+            // 
+            // soporteTecnicoToolStripMenuItem
+            // 
+            this.soporteTecnicoToolStripMenuItem.Name = "soporteTecnicoToolStripMenuItem";
+            this.soporteTecnicoToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.soporteTecnicoToolStripMenuItem.Text = "Soporte Tecnico";
+            // 
             // Reporte_Detallado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 470);
+            this.ClientSize = new System.Drawing.Size(795, 470);
             this.Controls.Add(this.btncomentario);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtcomentario);
@@ -280,17 +249,15 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Reporte_Detallado";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reporte Detallado";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Reporte_Detallado_FormClosed);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -299,14 +266,6 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem modulosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem tempraturaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem vientoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem humedadToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem presionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem mesaDeAyudaToolStripMenuItem;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txttemperatura;
         private System.Windows.Forms.TextBox txtpresion;
@@ -321,5 +280,10 @@
         private System.Windows.Forms.Button btncomentario;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtcomentario;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportarUnProblemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mesaDeAyudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proporcionarUnaSugerenciaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem soporteTecnicoToolStripMenuItem;
     }
 }
