8cda8ea9849003bb3605ac0009b329206dbe8f1e
thech <thech@HOMEFRONT>
2017-04-07 09:52:04 -0600

    cosas cososas

--- ClimateReports/ClimateReports/Presion.Designer.cs
+++ ClimateReports/ClimateReports/Presion.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
@@ -37,6 +38,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtpresionat = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,18 +46,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 32);
+            this.label1.Size = new System.Drawing.Size(93, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Presion";
             // 
             // btnimprimir
             // 
-            this.btnimprimir.Location = new System.Drawing.Point(408, 103);
-            this.btnimprimir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnimprimir.Location = new System.Drawing.Point(306, 84);
+            this.btnimprimir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnimprimir.Name = "btnimprimir";
-            this.btnimprimir.Size = new System.Drawing.Size(69, 27);
+            this.btnimprimir.Size = new System.Drawing.Size(52, 22);
             this.btnimprimir.TabIndex = 1;
             this.btnimprimir.Text = "Imprimir";
             this.btnimprimir.UseVisualStyleBackColor = true;
@@ -63,10 +66,10 @@
             // 
             // btnenviar
             // 
-            this.btnenviar.Location = new System.Drawing.Point(483, 103);
-            this.btnenviar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnenviar.Location = new System.Drawing.Point(362, 84);
+            this.btnenviar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnenviar.Name = "btnenviar";
-            this.btnenviar.Size = new System.Drawing.Size(75, 27);
+            this.btnenviar.Size = new System.Drawing.Size(56, 22);
             this.btnenviar.TabIndex = 2;
             this.btnenviar.Text = "Enviar";
             this.btnenviar.UseVisualStyleBackColor = true;
@@ -74,18 +77,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 103);
+            this.label2.Location = new System.Drawing.Point(27, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(103, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Presion Atmosferica:";
             // 
             // txtpresionat
             // 
-            this.txtpresionat.Location = new System.Drawing.Point(180, 103);
-            this.txtpresionat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtpresionat.Location = new System.Drawing.Point(135, 84);
+            this.txtpresionat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtpresionat.Name = "txtpresionat";
-            this.txtpresionat.Size = new System.Drawing.Size(163, 22);
+            this.txtpresionat.Size = new System.Drawing.Size(123, 20);
             this.txtpresionat.TabIndex = 4;
             // 
             // chart1
@@ -94,29 +98,33 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 214);
-            this.chart1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chart1.Location = new System.Drawing.Point(9, 174);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(545, 300);
+            this.chart1.Size = new System.Drawing.Size(409, 244);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chartpresion";
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Presion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(589, 526);
+            this.ClientSize = new System.Drawing.Size(442, 427);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.txtpresionat);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnenviar);
             this.Controls.Add(this.btnimprimir);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Presion";
             this.Text = "Presion";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
@@ -133,5 +141,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtpresionat;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
