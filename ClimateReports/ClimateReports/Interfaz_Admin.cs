9163a5164f0208216e83a1191d66db93d2e79660
Samuel Salgado Gonzalez <vesuvianmetal@hotmail.com>
2017-06-02 09:55:14 -0700

    validacion agregar contraseña validado

--- ClimateReports/ClimateReports/Interfaz_Admin.cs
+++ ClimateReports/ClimateReports/Interfaz_Admin.cs
@@ -380,5 +380,21 @@ namespace ClimateReports
         {
 
         }
+
+        private void txtcontraseña_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+       (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+           
+        }
     }
 }
